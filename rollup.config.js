a61c134361dde77e66db80495c8ff4f0748f66b8
givensuman <givensuman@gmail.com>
2022-04-01 14:57:31 -0400

    Publishing configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,24 @@
 import typescript from "rollup-plugin-typescript2"
+import css from "rollup-plugin-import-css"
 import pkg from "./package.json"
 
 export default [
   {
-    input: "src/index.ts",
+    input: "src/index.tsx",
     external: Object.keys(pkg.peerDependencies || {}),
     plugins: [
       typescript({
         typescript: require("typescript")
-      })
+      }),
+      css()
     ],
     output: [
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "esm" },
       {
-        file: "example/src/reactComponentLib/index.js",
-        format: "es",
-        banner: "/* eslint-disable */"
-      }
-    ]
+        file: 'dist/index.js',
+        format: 'umd',
+        name: 'index',
+        sourcemap: 'inline',
+      },
+    ],
   }
 ]
